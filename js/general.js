dc590885214e0f2e77d787c6f82c119a788644dd
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-06-18 22:56:47 +0530

    added functionality to append the data on click

--- js/general.js
+++ js/general.js
@@ -1,6 +1,13 @@
 // popup the suggestion bar
+
+  var checkEmpty = false;
   $('#question').on('click', function () {
     $('#suggestion').css("display", "block");
+     if ($('#question-area').val() == "") {
+      checkEmpty = true;
+    }
+    else
+      checkEmpty = false;
   });
 
   var suggestionValue1 = "Post your Profile: \nGRE/GMAT/XYZ = \nAcademic Qualification: \nWork Experience: \nInternships: \nResearch Papers: \nPatents: \nAdditional Information: "
@@ -9,25 +16,41 @@
   var suggestionValue4 = "Applied Date: DD/MM/YYYY "
 
   $('#suggestion-btn1').on('click', function () {
+    if (checkEmpty == true) {
     $('#question-area').val("");
     $('#question-area').val(suggestionValue1);
     $('#suggestion').css("display", "none");
+    }
+    else
+      $('#question-area').val( $('#question-area').val() + "\n \n" + suggestionValue1);
   });
 
   $('#suggestion-btn2').on('click', function () {
+    if (checkEmpty == true) {
     $('#question-area').val("");
     $('#question-area').val(suggestionValue2);
     $('#suggestion').css("display", "none");
+    }
+    else
+      $('#question-area').val( $('#question-area').val() + "\n \n" + suggestionValue2);
   });
 
   $('#suggestion-btn3').on('click', function () {
+    if (checkEmpty == true) {
     $('#question-area').val("");
     $('#question-area').val(suggestionValue3);
     $('#suggestion').css("display", "none");
+    }
+    else
+      $('#question-area').val( $('#question-area').val() + "\n \n" + suggestionValue3);
   });
 
   $('#suggestion-btn4').on('click', function () {
+    if (checkEmpty == true) {
     $('#question-area').val("");
     $('#question-area').val(suggestionValue4);
     $('#suggestion').css("display", "none");
+    }
+    else
+      $('#question-area').val( $('#question-area').val() + "\n \n" + suggestionValue4);
   });
